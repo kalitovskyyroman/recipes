e2fd1ac2245b0a68080aba506f32a45db1c2ef91
Roman Kalitovskyy <rkalitovskyy@leobit.com>
2022-02-07 22:05:31 +0200

    CD: Add cd workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
-    console.log("res");
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-};
+const App = () => (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+            </a>
+        </header>
+    </div>
+);
 
 export default App;
