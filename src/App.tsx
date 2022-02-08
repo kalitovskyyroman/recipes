1f4c146cd47bed255e1e8c2e05d949db74399462
Roman Kalitovskyy <rkalitovskyy@leobit.com>
2022-02-08 14:37:09 +0200

    Stylelint: Add base configuration for styleling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
-import "./App.css";
+import style from "./App.module.scss";
 
 const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.app}>
+        <header className={style["app-header"]}>
+            <img src={logo} className={style["app-logo"]} alt="logo" />
             <p>
                 Edit <code>src/App.tsx</code> and save to reload.
             </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            <a className={style["app-link"]} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                 Learn React
             </a>
         </header>
